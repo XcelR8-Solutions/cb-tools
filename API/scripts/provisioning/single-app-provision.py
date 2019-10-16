831901cefc3b1646aad8263fce41dbcc10d49ec7
Jeff Brassard <jjbrassa@JB-MacBook-Pro.local>
2019-10-16 12:35:16 -0600

    added single app deploy sample

---
+++ API/scripts/provisioning/single-app-provision.py
@@ -0,0 +1,68 @@
+##################################################################
+# 
+# File: 
+# application-provision.py
+#
+# Desc: 
+# -------------------
+# Example for application provisioning via the Morpheus API
+#
+# Requirements:
+# -------------------
+# python3 -m venv morpheus-tools
+# source morpheus-tools/bin/activate
+# pip install requests
+#
+#
+# Steps:
+# -------------------
+# 1. API, Auth
+# 2. API, Get cloud details
+# 3. API, Get app blueprint details
+# 4. API, Create app
+#
+##################################################################
+
+import json
+import requests
+import urllib
+import sys
+import os
+
+# Set vars
+# TODO Move this into a config file
+appPath = os.path.abspath(os.path.dirname(__file__) + '../..')
+resourcePath = appPath + "/resources"
+appBpPath = resourcePath + "/CICD-lab-app-config.json"
+awsCloudCode = "mtaws"
+
+sys.path.append(appPath)
+from morphapi.apps import App
+from morphapi.cloud import Cloud
+
+# 1. Build objects and print config
+appObj = App()
+cloudObj = Cloud()
+
+appObj.printConfig()
+
+###### 
+# 1. Get cloud details
+######
+cloudObj.out("Getting cloud info for ["+awsCloudCode+"]")
+zone = cloudObj.getZone(awsCloudCode)
+
+###### 
+# 2. Get app blueprint
+###### 
+appObj.out("Getting Application Blueprint...")
+print(appBpPath)
+appBP = appObj.getAppBP(appBpPath)
+
+###### 
+# 9. Create App
+###### 
+appObj.out("Building application...");
+appResponse = appObj.runAppCreate(appBP)
+print(appResponse);
+
