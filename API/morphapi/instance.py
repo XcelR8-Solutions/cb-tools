3a0cbbe882ef5449d036a7be5e2b7a191cb2120d
Jeff Brassard <jjbrassa@JB-MacBook-Pro.local>
2019-10-18 17:23:06 -0600

    new stuff and re-org

---
+++ API/morphapi/instance.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from .base import APIBase
+
+class Instance(APIBase):
+	
+	def __init__(self):
+		APIBase.__init__(self)
+
+	def getInstances(self):
+		url = self.apiEndpoint+"/api/instances"
+		response = requests.get(url, verify=False, headers=self.headers)
+		return json.loads(response.content);
+
+	def getInstanceByName(self, instanceName):
+		matchInstance = ""
+		url = self.apiEndpoint+"/api/instances"
+		response = requests.get(url, verify=False, headers=self.headers)
+		iList = json.loads(response.content);
+		for instance in iList.get('instances'):
+			if instance['name'].strip() == instanceName.strip():
+				matchInstance = instance;
+				break
+		return matchInstance; 
--- API/scripts/instances/get-instances.py
+++ API/scripts/instances/get-instances.py
@@ -1,63 +1,47 @@
-import json
-import requests
-import urllib
-import os
-
+##################################################################
+# 
+# File: 
+# get-instances.py
+#
+# Desc: 
+# -------------------
+# Example for retrieving instance information
+#
+# Requirements:
 # -------------------
 # python3 -m venv morpheus-tools
 # source morpheus-tools/bin/activate
 # pip install requests
 #
+#
 # Steps:
 # -------------------
-# 1. Call API, auth, get bearer token
-# 2. Call API, get instances in a cloud
-##################################################################
-
-# Define vars
-apiEndpoint = os.environ['MORPHEUS_URL']
-apiUsername = os.environ['MORPHEUS_USERNAME']
-apiPassword = os.environ['MORPHEUS_PASSWORD']
-
-# Disable ssl warnings (for my example)
-requests.packages.urllib3.disable_warnings()
-
-# Auth()
-def auth():
-	url = apiEndpoint+"/oauth/token?grant_type=password&scope=write&client_id=morph-customer"
-	credsJSON = {'username':apiUsername, 'password': apiPassword};
-	response = requests.post(url, verify=False, headers='', data=credsJSON)
-	authInfo = json.loads(response.content)
-	return authInfo.get('access_token');
-
-# GetInstanceByName()
-def getInstanceByName(instanceName):
-	matchInstance = ""
-	url = apiEndpoint+"/api/instances"
-	response = requests.get(url, verify=False, headers=headers)
-	iList = json.loads(response.content);
-	for instance in iList.get('instances'):
-		if instance['name'].strip() == instanceName.strip():
-			matchInstance = instance;
-			break
-	return matchInstance; 
-
-# 1. Auth and get bearer token
-apiBearerToken = auth()
-headers = {'Content-Type': 'application/json','Authorization': 'Bearer {0}'.format(apiBearerToken)}
-
-# 2. Get instances in a cloud
-instance = getInstanceByName('jb-tomcat-1003')
-print("IP_ADDR="+instance['connectionInfo'][0]['ip'])
-
-##################################################################
-#
-# DOCS: https://docs.morpheusdata.com/en/4.0.0/provisioning/automation/automation.html#task-results
-#
-# To access the above variable in a script (same workflow phase, after this script)
-# 1. Set the return type for this task to "Key/Value Pairs"
-# 2. In next script output something to this affect:  echo "keyval value: <%=results.keyval.IP_ADDR%>"
+# 1. API, Auth
+# 2. API, Get instance list
 #
 ##################################################################
 
+import json
+import requests
+import urllib
+import sys
+import os
 
+# Set vars
+# TODO Move this into a config file
+appPath = os.path.abspath(os.path.dirname(__file__) + '../..')
+
+sys.path.append(appPath)
+from morphapi.instance import Instance
+
+# 1. Build objects and print config
+instanceObj = Instance()
+
+instanceObj.printConfig()
+
+###### 
+# 1. Get instances
+######
+instanceObj.out("Getting instances")
+instances = instanceObj.getInstances()
+print(instances)
