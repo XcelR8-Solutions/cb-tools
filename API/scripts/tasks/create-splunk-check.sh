b6241d1ffaba4d02a6c33ed8d76ddad8efaf2f5a
Jeff Brassard <jjbrassa@JB-MacBook-Pro.local>
2019-12-17 17:55:24 -0800

    new monitor

---
+++ API/scripts/tasks/create-splunk-check.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+#
+# Builds a web monitor
+#
+
+MORPHEUS_URL="https://10.30.20.17"
+
+##
+# Authenticate
+## 
+AUTH_TOKEN=$(curl -k -X POST --data "username=$MORPHEUS_USERNAME&password=$MORPHEUS_PASSWORD" "$MORPHEUS_URL/oauth/token?grant_type=password&scope=write&client_id=morph-customer")
+ACCESS_TOKEN=$(sed -E 's/.*"access_token":"?([^,"]*)"?.*/\1/' <<< $AUTH_TOKEN)
+
+##
+# Grab vars
+##
+INTERNAL_IP="<%=server.internalIp%>"
+EXTERNAL_IP="<%=server.externalIp%>"
+SERVER_NAME="<%=server.name%>"
+
+##
+# Echo Bits
+##
+echo "internalip=$INTERNAL_IP"
+echo "externalip=$EXTERNAL_IP"
+echo "token=$ACCESS_TOKEN"
+echo "server_name=$SERVER_NAME"
+echo "morpheus_url=$MORPHEUS_URL"
+
+##
+# Splunk API Check : Build and make call
+##
+CHECK_NAME="Splunk API Check [$SERVER_NAME]"
+CHECK_URL="https://$EXTERNAL_IP:8089/services/server/info"
+
+curl -k -XPOST "$MORPHEUS_URL/api/monitoring/checks" \
+  -H "Authorization: BEARER $ACCESS_TOKEN" \
+  -H "Content-Type: application/json" \
+  -d "{'check':{
+    'name': '$CHECK_NAME',
+    'checkType': {'code': 'webGetCheck'},
+    'inUptime': true,
+    'severity': 'critical',
+    'description': null,
+    'checkInterval': 300,
+    'checkAgent': null,
+    'active': true,
+    'config': {
+      'checkUser':'admin',
+      'checkPassword':'n+fY%bqb3nbwBnbx',
+      'webMethod': 'GET',
+      'webUrl': '$CHECK_URL'
+    }
+  }}"
