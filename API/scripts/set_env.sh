3a5463e7d53669693e99b3c263544e07e3b9e54b
Jeff Brassard <jjbrassa@JB-MacBook-Pro.local>
2020-02-17 15:16:49 -0700

    env setting bash file

---
+++ API/scripts/set_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###
+# Sets params for env
+###
+export MORPHEUS_URL="*URL*"
+export MORPHEUS_USERNAME="*USERNAME*"
+export MORPHEUS_PASSWORD="*PASSWORD*"
+
+echo ""
+echo "Morpheus Details:"
+echo "-----------------"
+echo "URL=$MORPHEUS_URL"
+echo "USERNAME=$MORPHEUS_USERNAME"
+echo "PASSWORD=$MORPHEUS_PASSWORD"
+echo ""
