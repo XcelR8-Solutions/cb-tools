205f866c0a1aa96ef0cbdc3ee3eb220dfa74722a
Jeff Brassard <jbrassard@jbrassard-mbp13.local>
2021-04-28 21:56:27 -0600

    some wordpress shit

---
+++ actions/sh/ubuntu-install-mysql.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+dbname='{{server.database_name}}'
+dbuser='{{server.database_username}}'
+dbpass='{{server.database_password}}'
+wphost='{{blueprint_context.wordpress_server.servers.0.ip}}'
+
+echo "Installing mysql server"
+echo "============================================"
+echo " -> | dbname - $dbname"
+echo " -> | dbuser - $dbuser"
+echo " -> | dbpass - $dbpass"
+echo " -> | wphost - $wphost"
+echo "============================================"
+apt install -y mysql-server
+
+echo " -> Starting mysql server (if not already started)"
+systemctl start mysql.service
+
+echo " -> Creating database with the name '$dbname'..."
+mysql -u root <<-EOSQL
+  CREATE DATABASE $dbname;
+  CREATE USER '${dbuser}'@'localhost' IDENTIFIED BY '${dbpass}';
+  GRANT ALL PRIVILEGES ON $dbname.* TO '${dbuser}'@'${wphost}';
+  FLUSH PRIVILEGES;
+EOSQL
+
+# allow login from wordpress server
+#cat <<EOF >> /etc/my.cnf
+#[mysqld]
+#bind-address=0.0.0.0
+#EOF
+
+echo " -> MySQL server installation completed."
---
+++ actions/sh/ubuntu-install-wordpress.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+dbname='{{server.database_name}}'
+dbuser='{{server.database_username}}'
+dbpass='{{server.database_password}}'
+dbhost='{{blueprint_context.database_server.servers.0.ip}}'
+
+echo "WordPress Install Script"
+echo "============================================"
+echo "| dbname - $dbname"
+echo "| dbuser - $dbuser"
+echo "| dbpass - $dbpass"
+echo "| wphost - $wphost"
+echo "============================================"
+
+# wordpress prereqs
+python -mplatform | grep -qi ubuntu
+is_ubuntu=$?
+
+if [ $is_ubuntu -eq 0 ]; then
+    apt-get install apache2 php5 libapache2-mod-php5 php5-mysql php5-mysqlnd curl debconf-utils
+else
+    yum install -y httpd php php-common php-mysql php-gd php-xml php-mbstring php-mcrypt 
+fi
+
+# download wordpress
+curl -O https://wordpress.org/latest.tar.gz
+# unzip wordpress
+tar -zxf latest.tar.gz
+# change dir to wordpress
+cd wordpress
+
+#create wp config
+cp wp-config-sample.php wp-config.php
+#set database details with perl find and replace
+perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+perl -pi -e "s/localhost/$dbhost/g" wp-config.php
+
+#set WP salts
+perl -i -pe'
+  BEGIN {
+    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+  }
+  s/put your unique phrase here/salt()/ge' wp-config.php
+
+#create uploads folder and set permissions
+mkdir -p wp-content/uploads
+chmod 775 wp-content/uploads
+
+echo "Copying WordPress files to /var/www/html..."
+cp -r ~/wordpress/* /var/www/html
+service httpd restart
+
+echo "Cleaning..."
+rm ../latest.tar.gz
+
+echo "========================="
+echo "Installation is complete."
+echo "========================="
