384aeb490527222ec6a548304b9e0a7f73c11fa8
Jeff Brassard <jbrassard@jbrassard-mbp13.local>
2021-07-06 19:23:52 -0600

    new shit

--- actions/powershell/CB-Launch-Blueprint.ps1
+++ actions/powershell/CB-Launch-Blueprint.ps1
@@ -1,2 +1,80 @@
-﻿echo "Hello Butthole"
-#Invoke-RestMethod 
+﻿echo "--------------------------------------------------"
+echo "CloudBolt REST API Blueprint Example!"
+echo "--------------------------------------------------"
+
+###
+# 1. Auth and get bearer token
+###
+echo "Auth against API..."
+$Headers = @{
+    'Accept' = 'application/json'
+    'Content-Type' = 'application/json'
+}
+
+$Body = @{
+   'username' = 'jbrassard'
+   'password' = 'D00kie4me!!'
+}
+ 
+$Parameters = @{
+    Method = "Post"
+    Uri =  "https://cloudbolt-local.com/api/v2/api-token-auth/"
+    Body = ($Body | ConvertTo-Json) 
+    ContentType = "application/json"
+    Headers = $Headers
+}
+
+$Response = Invoke-RestMethod -SkipCertificateCheck @Parameters
+$BearerToken = $Response.token
+
+###
+# 2. Hit API and fire up workload!
+###
+echo "Calling API To Create BluePrint..."
+$BPCreate = @'
+{
+    "group": "/api/v2/groups/GRP-4ocssk7c/",
+    "items": {
+        "deploy-items": [
+            {
+                "blueprint": "/api/v2/blueprints/BP-df7476du/",
+                "blueprint-items-arguments": {
+                    "build-item-Ubuntu-18.04-Build": {
+                        "attributes": {
+                            "quantity": 1
+                        },
+                        "environment": "/api/v2/environments/ENV-zsxxio85/",
+                        "os-build": "/api/v2/os-builds/OSB-2qeptzs2/",
+                        "parameters": {
+                            "app-name": "App Server",
+                            "cost-center": "Finance",
+                            "service-tier": "24x7x7"
+                        }
+                    }
+                },
+                "resource-name": "ubuntu1804",
+                "resource-parameters": {}
+            }
+        ]
+    },
+    "submit-now": "true"
+}
+'@
+ 
+$Headers2 = @{
+    'Accept' = 'application/json'
+    'Content-Type' = 'application/json'
+    'Authorization' = "Bearer $BearerToken"
+}
+
+$Parameters2 = @{
+    Method = "Post"
+    Uri =  "https://cloudbolt-local.com/api/v2/orders/"
+    ContentType = "application/json"
+    Headers = $Headers2
+    Body = $BPCreate
+}
+
+Invoke-RestMethod -SkipCertificateCheck @Parameters2
+
+
---
+++ actions/sh/ubuntu-install-jenkins.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Install jenkins!
+
+myServer='{{server.ip}}'
+
+echo "Installing java..."
+apt update 
+apt install -y openjdk-8-jdk openjdk-8-jre
+cat >> /etc/environment <<EOL
+JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+EOL
+
+echo "Installing jenkins..."
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt update
+apt install -y jenkins
+
+echo "Firing jenkins UP!..."
+systemctl start jenkins
+systemctl status jenkins
+
+echo "Your jenkins box [$myServer:8080]"
+myInitPswd=$( cat /var/lib/jenkins/secrets/initialAdminPassword )
+echo "Your initialAdminPassword [$myInitPswd]"
