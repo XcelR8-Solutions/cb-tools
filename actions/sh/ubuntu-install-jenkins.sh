8975bbf0fdba0031d7f6d0973bd3e316f3356667
Jeff Brassard <jbrassard@jbrassard-mbp13.local>
2021-07-07 18:03:17 -0600

    maybe?

--- actions/sh/ubuntu-install-jenkins.sh
+++ actions/sh/ubuntu-install-jenkins.sh
@@ -7,7 +7,6 @@ myServer='{{server.ip}}'
 echo "--------------------------------------------"
 echo "| Installing java..."
 echo "--------------------------------------------"
-apt update 
 apt install -y openjdk-8-jdk openjdk-8-jre
 cat >> /etc/environment <<EOL
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
@@ -15,15 +14,11 @@ JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
 EOL
 source /etc/environment
 
-echo "Sleep for 30s..."
-sleep 30
-
 echo "--------------------------------------------"
 echo "| Installing jenkins..."
 echo "--------------------------------------------"
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-apt update
 apt install -y jenkins
 
 echo "--------------------------------------------"
